d2ff0508e06956caa3e72745350925835b6e19bb
Lucas Talamini <lucastalamini@hotmail.com>
2019-07-28 16:33:52 -0300

    Forecast message update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,9 +8,11 @@ const forecast = (latitude, longitude, callback) => {
         callback('Unable to connect to weather services!', undefined)
     } else if (body.error) {
         callback('Unable to find location.', undefined)
+    } else if ((parseFloat(body.currently.precipProbability) * 100) > 40) {
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' °C out. There is a ' +  (parseFloat(body.currently.precipProbability) * 100) + '% chance of rain. Consider bringing an umbrella.')
     } else {
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' °C out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-    } 
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' °C out. There is a ' +  (parseFloat(body.currently.precipProbability) * 100) + '% chance of rain.')
+    }
 })
 }
 
